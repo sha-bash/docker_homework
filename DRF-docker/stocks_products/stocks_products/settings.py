45dcb44b3dfc78e7ff44626f43b6f69382426a16
sha-bash <v.shabashev@automacon.ru>
2024-10-07 23:46:23 +0400

    homework docker

--- DRF-docker/stocks_products/stocks_products/settings.py
+++ DRF-docker/stocks_products/stocks_products/settings.py
@@ -85,14 +85,21 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'cruddb',
+#         'USER': 'postgres',
+#         'PASSWORD': '7Ff3imRYmE',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cruddb',
-        'USER': 'postgres',
-        'PASSWORD': '7Ff3imRYmE',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
